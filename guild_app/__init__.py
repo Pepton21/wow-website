b3adac5c5e4bbeb9520cb7c76b7af51c7891cb16
Petar Tonkovic <tonkovic.petar@gmail.com>
2018-05-23 21:55:54 +0200

    Fixed outdated app name

--- guild_app/__init__.py
+++ guild_app/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 import guild_app.views
-from guild_app.database import db_session
+"""from guild_app.database import db_session
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     if db_session != None:
-        db_session.close()
+        db_session.close()"""
