c0e396684db50d8c83467a559b348abf51d3833e
Petar Tonkovic <tonkovic.petar@gmail.com>
2018-05-23 21:38:03 +0200

    Fixed outdated app name

--- guild_app/__init__.py
+++ guild_app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 import cmdb_app.views
-from cmdb_app.database import db_session
+from guild_app.database import db_session
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
